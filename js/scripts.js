c05976066d15dabff128f83c7ef6b0c3391842b4
PolinaSt4 <polina.steinmueller@yahoo.com>
2024-11-23 14:47:28 +0100

    CF F A2.3 Call review

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
-alert("Hello Nice Person, I am Polina, UX Designer");
-console.log("Hello Nice Person, I am Polina, UX Designer")
-
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
-    }
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+    document.addEventListener('click', outsideClickListener);
+  } else {
+    x.className = 'navtoggle';
+    document.removeEventListener('click', outsideClickListener);
   }
+}
+
+function outsideClickListener(event) {
+  var menu = document.getElementById('myNavtoggle');
+  if (!menu.contains(event.target) && menu.className.includes('responsive')) {
+    menu.className = 'navtoggle'; // Close the menu
+    document.removeEventListener('click', outsideClickListener); // Remove the listener
+  }
+}

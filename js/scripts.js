65cf5740dce0ac12a480e8b3e16d836d7b223cac
PolinaSt4 <polina.steinmueller@yahoo.com>
2024-11-23 11:29:28 +0100

    CF F A2.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,11 @@
 alert("Hello Nice Person, I am Polina, UX Designer");
 console.log("Hello Nice Person, I am Polina, UX Designer")
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
